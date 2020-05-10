7a3c13daa01d8750b7ef00b8be3e061aab9a97a7
Oh Chin Yang <iamohcy@gmail.com>
2020-05-10 20:35:52 +0800

    removed delete button

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('submit_consent', views.submit_consent, name='submit_consent'),
     path('submit_images', views.submit_images, name='submit_images'),
     path('complete_submission', views.complete_submission, name='complete_submission'),
-    path('delete_entry', views.delete_entry, name='delete_entry'),
+    # path('delete_entry', views.delete_entry, name='delete_entry'),
 ]
