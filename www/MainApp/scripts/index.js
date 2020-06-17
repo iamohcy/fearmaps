af8bb47680976b29e36444c817931962c1af32a7
Oh Chin Yang <iamohcy@gmail.com>
2020-06-17 13:45:59 +0800

    Added tooltip for base visualization

--- www/MainApp/scripts/index.js
+++ www/MainApp/scripts/index.js
@@ -7,5 +7,7 @@ $(function() {
     tl.to(".blended_image", {opacity: 1, duration: DURATION, ease: "power0"});
     tl.to(".image1", {opacity: 1, duration: DURATION, ease: "power0"});
 
+    $('[data-toggle="tooltip"]').tooltip();
+
 });
 
