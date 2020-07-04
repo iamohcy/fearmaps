2c1ed6e03e813605d622c5d374e1b98b0ade2f95
Oh Chin Yang <iamohcy@gmail.com>
2020-07-04 23:19:42 +0800

    Fixed issue with text not being replaced and the wrong image being replaced in the animation cycle.

--- www/MainApp/scripts/viz1.js
+++ www/MainApp/scripts/viz1.js
@@ -33,35 +33,56 @@ $(function() {
             [1,1,1],
         ]
 
-        var replacementIndex = 0;
+        var replacementIndex = -1;
         var DURATION = 5;
+        // var DURATION = 1;
         var tl = gsap.timeline({repeat: -1, repeatDelay: 0, onRepeat:function(){
+            console.log("REPEAT!");
+            replacementIndex = (replacementIndex + 1) % MAX_NUM_SAMPLES;
+
             // console.log(allFearItems.length, previousFearItems.length);
         }});
+
+        var checkForReplace = function(imageIndex,opacity) {
+
+            console.log(imageIndex, opacity, replacementIndex);
+            // Replace the image only when it's opacity has reached 0
+            if (imageIndex === replacementIndex && opacity <= 0) {
+                console.log("CHANGE! " + replacementIndex);
+                // We've emptied the array, replace it back with the old popped items
+                if (allFearItems.length == 0) {
+                    allFearItems = previousFearItems;
+                    previousFearItems = [];
+                }
+
+                var newFearItem = getRandomItem(allFearItems);
+                previousFearItems.push(newFearItem);
+                blendedFearItems[replacementIndex] = newFearItem;
+                $('.image-'+replacementIndex).attr('src',"/media/" + newFearItem.fields.image_1_tb);
+                $("#image-" + replacementIndex + "-text").html(newFearItem.fields.fear_text);
+            }
+
+
+            // replacementIndex = (replacementIndex + 1) % MAX_NUM_SAMPLES;
+        }
+
         for (var i = 0; i < opacityCombinations.length; i++) {
             var combination = opacityCombinations[i];
             for (var j = 0; j < MAX_NUM_SAMPLES; j++) {
-                tl.to("#blend-image-" + j, {opacity: combination[j], duration: DURATION, ease: "power0"}, i * DURATION);
+                tl.to("#blend-image-" + j, {
+                    opacity: combination[j],
+                    duration: DURATION,
+                    ease: "power0",
+                    onComplete: checkForReplace,
+                    onCompleteParams: [j, combination[j]],
+                }, i * DURATION);
+                // tl.add( function(){
+
+                // } )
             }
 
-            // Second last one, change first image!
-            if (opacityCombinations[i][replacementIndex] == 0) {
-                tl.add( function(){
-                    // We've emptied the array, replace it back with the old popped items
-                    if (allFearItems.length == 0) {
-                        allFearItems = previousFearItems;
-                        previousFearItems = [];
-                    }
-
-                    var newFearItem = getRandomItem(allFearItems);
-                    previousFearItems.push(newFearItem);
-                    blendedFearItems[replacementIndex] = newFearItem;
-                    $('.image-'+replacementIndex).attr('src',"/media/" + newFearItem.fields.image_1_tb);
-                    $("#image-" + i + "-text").html(newFearItem.fields.fear_text);
-
-                    replacementIndex = (replacementIndex + 1) % MAX_NUM_SAMPLES;
-                } )
-            }
+            // if (opacityCombinations[i][replacementIndex] == 0) {
+            // }
         }
     });
 
